3e900aff0a0d149c443edf1a2e093cbf59f2e822
IsmailDX <ismaildxx@gmail.com>
2025-10-21 17:46:36 +0400

    adding 3d models into the section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
+import ProductViewer from './components/ProductViewer'
 
 const App = () => {
   return (
     <main>
         <NavBar />
         <Hero />
+        <ProductViewer />
     </main>
   )
 }
